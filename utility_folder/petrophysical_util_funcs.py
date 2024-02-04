fb178372f7a5063c44e50dca162a43ce56070c1d
philusnarh <philusnarh@gmail.com>
2024-02-04 16:02:12 +0000

    sark script on ML added

---
+++ utility_folder/petrophysical_util_funcs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+T. A-N authored this Python script
+'''
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import norm, probplot
+from sklearn.preprocessing import QuantileTransformer
